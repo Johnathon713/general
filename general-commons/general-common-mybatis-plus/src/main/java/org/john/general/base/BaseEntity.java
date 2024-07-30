8230e5430c51beaa8f444f9c22114fd55fa9d331
Johnathon <Johnathon713@iCloud.com>
2024-07-30 23:14:08 +0800

    init

--- general-commons/general-common-mybatis-plus/src/main/java/org/john/general/base/BaseEntity.java
+++ general-commons/general-common-mybatis-plus/src/main/java/org/john/general/base/BaseEntity.java
@@ -1,7 +1,10 @@
 package org.john.general.base;
 
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.Version;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.john.general.user.domain.User;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -10,6 +13,7 @@ import java.util.Date;
  * @author Johnathon
  */
 @Data
+@NoArgsConstructor
 public class BaseEntity implements Serializable {
     private Long createUserId;
     private String createUserName;
@@ -22,9 +26,46 @@ public class BaseEntity implements Serializable {
     @Version
     private Integer version;
     // 0为正常, 1为逻辑删除
-    private Integer deleted;
+    @TableLogic
+    private Integer deleted = 0;
     private Long deleteUserId;
     private String deleteUserName;
     private String deleteNickName;
     private Date deleteTime;
+
+    public BaseEntity(User createUser, User modifyUser) {
+        markCreateUser(createUser);
+        markModifyUser(modifyUser);
+    }
+
+    public BaseEntity(User user) {
+        this(user, user);
+    }
+
+    public void markCreateUser(User user) {
+        if (user != null) {
+            this.createUserId = user.getUserId();
+            this.createUserName = user.getUserName();
+            this.createNickName = user.getNickName();
+        }
+        this.createTime = new Date();
+    }
+
+    public void markModifyUser(User user) {
+        if (user != null) {
+            this.modifyUserId = user.getUserId();
+            this.modifyUserName = user.getUserName();
+            this.modifyNickName = user.getNickName();
+        }
+        this.modifyTime = new Date();
+    }
+
+    public void markDeleteUser(User user) {
+        if (user != null) {
+            this.deleteUserId = user.getUserId();
+            this.deleteUserName = user.getUserName();
+            this.deleteNickName = user.getNickName();
+        }
+        this.deleteTime = new Date();
+    }
 }
