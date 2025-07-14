18de38d2c15ff39652e581629f4072f67a2b81b7
llxx <Johnathon713@iCloud.com>
2025-07-14 11:39:11 +0800

    init

---
+++ snail-job/src/main/java/org/john/SnailJobServerApp.java
@@ -0,0 +1,15 @@
+package org.john;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author llxx
+ */
+@SpringBootApplication
+public class SnailJobServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SnailJobServerApp.class, args);
+    }
+}
