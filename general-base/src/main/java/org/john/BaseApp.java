2ef90e01874d8aba28945ca9b48b3de4b51b037a
llxx <Johnathon713@iCloud.com>
2025-07-14 15:09:59 +0800

    init

--- general-base/src/main/java/org/john/BaseApp.java
+++ general-base/src/main/java/org/john/BaseApp.java
@@ -2,11 +2,13 @@ package org.john;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author llxx
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BaseApp {
     public static void main(String[] args) {
         SpringApplication.run(BaseApp.class, args);
--- snail-job-server/src/main/java/org/john/SnailJobServerApp.java
+++ snail-job-server/src/main/java/org/john/SnailJobServerApp.java
@@ -3,11 +3,13 @@ package org.john;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author llxx
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SnailJobServerApp {
     public static void main(String[] args) {
         SpringApplication.run(SnailJobServerApp.class, args);
