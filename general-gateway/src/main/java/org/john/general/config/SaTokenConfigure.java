4fd9a6adae0a08a9d06626d15df084e914e16bd5
llxx <Johnathon713@iCloud.com>
2025-07-15 10:18:48 +0800

    init

--- general-gateway/src/main/java/org/john/general/config/SaTokenConfigure.java
+++ general-gateway/src/main/java/org/john/general/config/SaTokenConfigure.java
@@ -21,7 +21,7 @@ public class SaTokenConfigure {
                 // 拦截地址
                 .addInclude("/**")    /* 拦截全部path */
                 // 开放地址
-                .addExclude("/favicon.ico").addExclude("api/auth_service/**")
+                .addExclude("/favicon.ico").addExclude("/api/auth_service/**")
                 // 鉴权方法：每次访问进入
                 .setAuth(obj -> {
                     // 登录校验 -- 拦截所有路由，并排除/user/doLogin 用于开放登录
